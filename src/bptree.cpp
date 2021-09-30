bfecc5455ce17e0d78c8f3083ab97b3234ef55a6
codergan <ganx0005@e.ntu.edu.sg>
2021-09-30 21:25:27 +0800

    fix bugs

--- src/bptree.cpp
+++ src/bptree.cpp
@@ -37,39 +37,43 @@ void BPTree::removeInternal(uint32_t key) {
     int index;
     for(int i=0; i<keys.size(); i++){
         if (key == keys[i]) {
-            block = cur_node->getBlocks()[i];
+            vector<shared_ptr<Block>> block = cur_node->getBlocks()[i];
             found = true;
             index = i;
             break;
         }
     }
     if (!found){
-        print("key not found, cannot remove")
         return;
-    }else{
-        // call storage to remove that key
-        cur_node->keys.erase( cur_node->keys.begin() + index );
-        cur_node->blocks.erase( cur_node->blocks.begin() + index );
-        cur_node->numKeys --;
+    }else {
+        // call storage to remove that key using block
+        cur_node->keys.erase(cur_node->keys.begin() + index);
+        cur_node->blocks.erase(cur_node->blocks.begin() + index);
+        cur_node->numKeys--;
 
-        while(cur_node->numKeys+cur_node->preNode->numKeys <= cur_node->maxKeys || cur_node->numKeys+cur_node->nextNode->numKeys <= cur_node->maxKeys){
-            if (cur_node->numKeys + cur_node->preNode->numKeys <= cur_node->maxKeys){
+        while ((!cur_node->preNode && cur_node->numKeys + cur_node->preNode->numKeys <= cur_node->maxKeys) ||
+               (!cur_node->nextNode && cur_node->numKeys + cur_node->nextNode->numKeys <= cur_node->maxKeys)) {
+            if (cur_node->numKeys + cur_node->preNode->numKeys <= cur_node->maxKeys) {
                 cur_node = cur_node->preNode;
             }
-            vector<Node *> nodeList = cur_node->getParentPtr()->getChildNodes();
-            int index = distance(nodeList.begin(), cur_node);
+            vector < Node * > nodeList = cur_node->getParentPtr()->getChildNodes();
+            vector < Node * >::iterator it = std::find(nodeList.begin(), nodeList.end(), cur_node);
+
+            int index = distance(nodeList.begin(), it);
             cur_node->numKeys += cur_node->nextNode->numKeys;
             move(cur_node->nextNode->keys.begin(), cur_node->nextNode->keys.end(), std::back_inserter(cur_node->keys));
 
-            if (cur_node->isLeafNode()){
-                move(cur_node->nextNode->blocks.begin(), cur_node->nextNode->blocks.end(), std::back_inserter(cur_node->blocks));
-            }else{
-                move(cur_node->nextNode->pointers.begin(), cur_node->nextNode->pointers.end(), std::back_inserter(cur_node->pointers));
+            if (cur_node->isLeafNode()) {
+                move(cur_node->nextNode->blocks.begin(), cur_node->nextNode->blocks.end(),
+                     std::back_inserter(cur_node->blocks));
+            } else {
+                move(cur_node->nextNode->pointers.begin(), cur_node->nextNode->pointers.end(),
+                     std::back_inserter(cur_node->pointers));
             }
             cur_node->nextNode = cur_node->nextNode->nextNode;
             cur_node = cur_node->parentPtr;
-            }
         }
     }
-    return block;
+
+    return ;
 }
